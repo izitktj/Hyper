3695c038800ce8d8db28e7633eb47b2fd5147324
izitktj <universoreversoizi@gmail.com>
2024-09-07 11:04:01 -0300

    Começando a ficar menos feio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,42 @@
-const DiaDeHoje: string = "07/09/2024"
-
 function Cabecario()
 {
   return(
-    <div className="flex justify-between items-center bg-neutral-900 h-15 headers">
+    <div className="headers">
       <div>Hyper</div>
       <div className="flex flex-col justify-between items-center">
         <div className="titulo">Bem vindo a o Hyper!</div>
-        <div>{DiaDeHoje}</div>
+        <div>luagr de fazer sucesso</div>
       </div>
       <div>Login</div>
     </div>
   )
+}
+function SubCabecario()
+{
+  return(
+    <div style={SubHeader}>
+      <div>Vídeos engraçados</div>
+      <div>Jogos divertidos</div>
+      <div>Postagens de texto</div>
+      <div>Noticias</div>
+      <div>E muito mais</div>
+    </div>
+  )
+}
+
+const SubHeader = {
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  backgroundColor: '#2a2642',
+  color: '#e3c336'
 }
 
 export default function Home() {
   return (
     <main>
       <Cabecario/>
-      <h1>Hello, world!</h1>
+      <SubCabecario/>
     </main>
   );
 }
