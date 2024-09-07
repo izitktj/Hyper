a17a284ee65653fc40d598365f44ac2455b66c4c
izitktj <universoreversoizi@gmail.com>
2024-09-07 00:38:56 -0300

    Learning something

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,23 @@
+let DiaDeHoje: string = "07/09/2024"
+
+function Cabecario()
+{
+  return(
+    <div className="flex justify-between items-center bg-gray-300 h-15">
+      <div>Hyper</div>
+      <div className="flex flex-col justify-between items-center">
+        <div className="titulo">Bem vindo a o Hyper!</div>
+        <div>{DiaDeHoje}</div>
+      </div>
+      <div>Login</div>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <main>
+      <Cabecario/>
       <h1>Hello, world!</h1>
     </main>
   );
