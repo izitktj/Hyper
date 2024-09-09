874c50617875b4dd3ca0ee337906f0694089d750
izitktj <universoreversoizi@gmail.com>
2024-09-09 13:37:37 -0300

    upt cards

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,10 @@
 export default function Video(props: any) {
     return(
-        <div className="flex items-center flex-col border border-orange-600 rounded-md p-2">
-            <div className="text-gray-100">{props.nome}</div>
-            <div className="text-gray-600">{props.descricao}</div>
+        <div className="flex flex-col">
+            <div className="flex flex-col items-center border border-orange-600 rounded-md p-2 limit">
+                <div className="flex text-gray-100">{props.nome}</div>
+                <div className="flex text-gray-600">{props.descricao}</div>
+           </div>
         </div>
     )
 }
