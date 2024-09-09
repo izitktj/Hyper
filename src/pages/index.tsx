4e6a68b33599e54e581e4761614160e988b88894
izitktj <universoreversoizi@gmail.com>
2024-09-09 08:22:50 -0300

    Finalmente alinhado!!!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,15 +24,20 @@ const SubHeader = {
 
 export default function Home() {
   return (
-    <main className="bg-slate-950">
+    <main className="bg-slate-950 h-screen">
       <Topo/>
       <SubCabecario/>
-      <div className="flex flex-row p-4 gap-4">
-        <Lateral/>
-        <div className="flex justify-center flex-row gap-2">
-          <Video nome={'Receba v2'} descricao={'RECEBA CARACA!!!! #luva'}/>
-          <Video nome={"Carlinhos dando a bunda pros caras"} descricao={"shitpost"}/>
-          <Video nome={"Perdi o no fap tropa"} descricao={"#ff #fyp #soucarentemedaantencao"}/>
+      <div className="flex flex-row">
+        <div className="flex items-center place-items-center">
+          <Lateral/>
+        </div>
+
+        <div className="flex-1 flex justify-center">
+          <div className="flex flex-row gap-2">
+            <Video nome={'Receba v2'} descricao={'RECEBA CARACA!!!! #luva'}/>
+            <Video nome={"Carlinhos dando a bunda pros caras"} descricao={"shitpost"}/>
+            <Video nome={"Perdi o no fap tropa"} descricao={"#ff #fyp #soucarentemedaantencao"}/>
+          </div>
         </div>
       </div>
     </main>
