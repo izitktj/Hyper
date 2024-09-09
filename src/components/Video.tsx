9d8dca3f97e324f3b896f42d100cf05ce790db8c
izitktj <universoreversoizi@gmail.com>
2024-09-09 15:20:26 -0300

    Alinhamento e balançeamento

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,9 +1,9 @@
 export default function Video(props: any) {
     return(
         <div className="flex flex-col">
-            <div className="flex flex-col items-center border border-orange-600 rounded-md p-2 limit">
-                <div className="flex text-gray-100">{props.nome}</div>
-                <div className="flex text-gray-600">{props.descricao}</div>
+            <div className="flex flex-col border border-orange-600 rounded-md p-2 limit">
+                <div className="flex text-gray-100 items-center flex-auto">{props.nome}</div>
+                <div className="flex text-gray-600 flex-initial items-start">{props.descricao}</div>
            </div>
         </div>
     )
