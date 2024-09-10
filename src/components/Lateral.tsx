d7f17b975a4dd40e81e0389973b0ab149bd5b1d9
izitktj <universoreversoizi@gmail.com>
2024-09-10 11:45:47 -0300

    Barra lateral

--- src/components/Lateral.tsx
+++ src/components/Lateral.tsx
@@ -1,12 +1,12 @@
 export default function Lateral(props: any) {
     return(
-        <div className="flex flex-row gap-2">
-            <div className="flex flex-col align-text-top">
+        <div className="flex flex-row h-screen gap-2 bg-black px-6">
+            <div className="flex flex-col p-2">
                 <div className="text-white">Content</div>
-                <div className="object-top">{props.nomeAcc}</div>
+                <div className="text-white">{props.nomeAcc}</div>
             </div>
 
-            <div className="flex flex-row">
+            <div className="flex items-center place-items-center">
                 <div className="text-orange-400">X</div>
             </div>
         </div>
