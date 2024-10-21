bae93bc25aecd6c3e332dc68dcb1a86016bb6dc0
izitktj <universoreversoizi@gmail.com>
2024-10-21 13:38:00 -0300

    prevent no video name and desc

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,13 +1,13 @@
 interface VideoProps{
-    nome:string
-    descricao:string
+    nome?:string
+    descricao?:string
 }
 
 export default function Video(props: VideoProps) {
     return(
         <div className="flex flex-col border border-orange-600 rounded-md p-2 limit">
-            <div className="flex text-gray-100 items-center flex-auto">{props.nome}</div>
-            <div className="flex text-gray-600 flex-initial items-start">{props.descricao}</div>
+            <div className="flex text-gray-100 items-center flex-auto">{props.nome!==undefined ?( props.nome ):"Vídeo sem nome"}</div>
+            <div className="flex text-gray-600 flex-initial items-start">{props.descricao!==undefined ?( props.descricao ):"Vídeo sem descrição"}</div>
        </div>
     )
 }
