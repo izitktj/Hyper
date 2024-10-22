948c2bab6f5caa9a56058f278081d522d93b9c86
izitktj <universoreversoizi@gmail.com>
2024-10-22 11:30:20 -0300

    prevencao melhor 2

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,11 +3,19 @@ interface VideoProps{
     descricao?:string
 }
 
+function isEmpty(content: any){
+    if(content === undefined || content === "") {
+        return true
+    }
+
+    return false
+}
+
 export default function Video(props: VideoProps) {
     return(
         <div className="flex flex-col border border-orange-600 rounded-md p-2 limit">
-            <div className="flex text-gray-100 items-center flex-auto">{props.nome!==undefined ?( props.nome ):"Vídeo sem nome"}</div>
-            <div className="flex text-gray-600 flex-initial items-start">{props.descricao!==undefined ?( props.descricao ):"Vídeo sem descrição"}</div>
+            <div className="flex text-gray-100 items-center flex-auto">{!isEmpty(props.nome) ?( props.nome ):"Vídeo sem nome"}</div>
+            <div className="flex text-gray-600 flex-initial items-start">{!isEmpty(props.descricao) ?( props.descricao ):"Vídeo sem descrição"}</div>
        </div>
     )
 }
